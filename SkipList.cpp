6905fa7a3cfa2acf0dc8e5badb2b3e5437eb0612
Jilavu Izabela <izabelajilavu@gmail.com>
2024-04-23 22:27:16 +0300

    implemented randomization and insert method for SkipList

--- SkipList.cpp
+++ SkipList.cpp
@@ -1,7 +1,57 @@
 
 class SkipList : public BST {
+  private:
+    shared_ptr<Node> head;
+    shared_ptr<Node> tail;
+    const int maxLevel;
+
+    void randomize() {
+        int level = 0;
+
+        while ((double)rand() / RAND_MAX < 0.5 && level < maxLevel) level++;
+    
+        return level;
+    }
+
+  public:
+
+  SkipList() = default;
+
   void insert(T value) override {
-    // TODO
+    int newLevel = randomize();
+    shared_ptr<Node> newNode(new Node(value, newLevel));
+
+    if (head->nextNode[0] == tail) {
+      // if the list is empty
+
+      for (int i = 0; i < newLevel; i++) {
+        head->nextNode[i] = newNode;
+        newNode->nextNode[i] = tail;
+      }
+
+    } else {
+      // starting from the max level
+      
+      int currentLevel = maxLevel - 1;
+      shared_ptr<Node>currentNode = head;
+
+      while (currentNode >= 0) {
+
+        while (currentNode->nextNode[currentLevel] != tail && currentNode->nextNode[currentLevel]->data < value) {
+          currentNode = currentNode->nextNode[currentLevel];
+        }
+        
+        // check duplicates
+        if (currentNode->nextNode[currentLevel] != tail && currentNode->nextNode[currentLevel]->data == value) return;
+
+        if (currentLevel < newLevel) {
+          newNode->nextNode[currentLevel] = currentNode->nextNode[currentLevel];
+          currentNode->nextNode[currentLevel] = newNode;
+        }
+        
+        currentLevel--;
+      }
+    }
   }
 
   void remove(T value) override {
@@ -14,6 +64,11 @@ class SkipList : public BST {
 }
 
 class Node {
-  vector<shared_ptr<Node>>nextNodes;
-  int level;
+  private:
+    int data;  
+    int level;
+    map<int, shared_ptr<Node>>nextNode; // for each level, define the next node;
+  
+  public:
+    Node(int data, int level) : data(data), level(level){};
 }
