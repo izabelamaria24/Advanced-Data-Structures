daf69de0b646489995f6a5de53d90bcc9e52e8ba
Jilavu Izabela <83836468+izabelamaria24@users.noreply.github.com>
2024-05-01 13:28:00 +0300

    Added 2 more virtual methods in Structure.h

--- Structure.h
+++ Structure.h
@@ -3,9 +3,21 @@
 
 template<typename T>
 class Structure {
-  virtual void insert(T value) = 0;
+  // 1
+  virtual void insert(T value) = 0; 
+  // 2
   virtual void remove(T value) = 0;
+  // 3
   virtual void search(T value) = 0;
+  // 4
+  virtual void searchMaxValueBefore(T value) {
+    std::cout << "Search max value less than given value";
+  }
+  // 5 
+  virtual void searchMinValueAfter(T value) {
+    std::cout << "Search min value greater than given value";
+  }
+  // 6
   virtual void display() const {
     std::cout << "Display data structure";
   }
